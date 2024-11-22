aa9a7dd7eb13cdf19ac01efd88d2afd7720789fa
DemonHunter <82548410+DemonHunterBG@users.noreply.github.com>
2024-11-22 14:42:54 +0200

    Layer Separation

--- GameWebsite/GameWebsite/obj/Debug/net8.0/GameWebsite.AssemblyInfo.cs
+++ GameWebsite/GameWebsite/obj/Debug/net8.0/GameWebsite.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fb484b4ec080c8b3ded45413c78b8f36dda1604")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7ca8089da2db51355dc79f23165422c53b389f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
